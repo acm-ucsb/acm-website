71fa8f2f1db9a4e3a9806e0bbaef2d049912d288
rohankumar2025 <rohankumar@ucsb.edu>
2024-09-10 15:49:21 -0700

    added No Upcoming Events backup text

--- src/components/landing/body/EventsSection/EventsSection.tsx
+++ src/components/landing/body/EventsSection/EventsSection.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import EventCard from './EventCard';
 
 import { getEventsData } from '../../../../apis/getEventsData';
+import { Typography } from '@mui/material';
 
 const EventsSection = async () => {
 
@@ -12,9 +13,11 @@ const EventsSection = async () => {
         <div className="flex flex-col items-center justify-center mb-36">
             <h1 className="text-5xl font-bold mb-8 text-black">Upcoming Events</h1>
             <div className="flex flex-row flex-wrap justify-center">
-                {events?.map((event, index) => (
-                    <EventCard key={index} event={event} />
-                ))}
+                {events && events.length ?
+                    events?.map((event, index) => (
+                        <EventCard key={index} event={event} />
+                    )) : <Typography>No Upcoming Events!</Typography>
+                }
             </div>
         </div>
     );
