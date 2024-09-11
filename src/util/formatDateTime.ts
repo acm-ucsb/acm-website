451262ffc09c37cd9e4fb472167e06b0dd4b6577
rohankumar2025 <rohankumar@ucsb.edu>
2024-09-10 17:11:49 -0700

    ran prettier

--- src/util/formatDateTime.ts
+++ src/util/formatDateTime.ts
@@ -1,10 +1,14 @@
-import { format, parse } from 'date-fns';
+import { format, parse } from "date-fns";
 
-const formatDateTime = (datetime: string | undefined, targetFormat: string, fallbackString: string) => {
+const formatDateTime = (
+  datetime: string | undefined,
+  targetFormat: string,
+  fallbackString: string,
+) => {
   if (!datetime) return fallbackString;
 
   try {
-    const date = parse(datetime, 'yyyy-MM-dd HH:mm:ss', new Date());
+    const date = parse(datetime, "yyyy-MM-dd HH:mm:ss", new Date());
     return format(date, targetFormat);
   } catch (error) {
     return fallbackString;
