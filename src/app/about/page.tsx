009813d12bd456f32689a1c95234535d5db26e3f
Gabor Szita <gaborszita88@gmail.com>
2024-11-20 17:49:04 -0800

    fix prettier

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,188 +11,192 @@ import PageStyles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
+  const fake_data = [
+    [
+      {
+        id: "1",
+        name: "Foo Bar",
+        position: "Board",
+        img: Hero,
+      },
+      {
+        id: "2",
+        name: "Sam Shu",
+        position: "Board",
+        img: "/assets/test1.avif",
+      },
+      {
+        id: "2",
+        name: "Sam Shu",
+        position: "Board",
+        img: "/assets/test1.avif",
+      },
+    ],
+    [
+      {
+        id: "1",
+        name: "Foo Bar",
+        position: "Board",
+        img: Hero,
+      },
+    ],
+  ];
 
-    const fake_data = [[
-        {
-            id: "1",
-            name: "Foo Bar",
-            position: "Board",
-            img: Hero,
-        },
-        {
-            id: "2",
-            name: "Sam Shu",
-            position: "Board",
-            img: "/assets/test1.avif",
-        },
-        {
-            id: "2",
-            name: "Sam Shu",
-            position: "Board",
-            img: "/assets/test1.avif",
-        }
-    ], [
-        {
-            id: "1",
-            name: "Foo Bar",
-            position: "Board",
-            img: Hero,
-        },
-    ]];
+  return (
+    <main>
+      {/*Container to make top section full screen */}
+      <div className="relative h-screen flex flex-col">
+        <Navbar />
+        <Container className="grow" maxWidth="md">
+          {/*Center content*/}
+          <div className="h-[100%] flex flex-wrap justify-center content-center">
+            <div className="h-fit">
+              <div className="pb-[16vh]">
+                <Typography
+                  variant="h2"
+                  align="center"
+                  sx={{
+                    fontSize: "1.75rem",
+                    marginBottom: "1rem",
+                    fontWeight: "bold",
+                  }}
+                >
+                  What is ACM?
+                </Typography>
 
-    return (
-        <main>
-            {/*Container to make top section full screen */}
-            <div className="relative h-screen flex flex-col">
-                <Navbar />
-                <Container className="grow" maxWidth="md">
-                    {/*Center content*/}
-                    <div className="h-[100%] flex flex-wrap justify-center content-center">
-                        <div className="h-fit">
-                            <div className="pb-[16vh]">
-                                <Typography
-                                    variant="h2"
-                                    align="center"
-                                    sx={{
-                                        fontSize: "1.75rem",
-                                        marginBottom: "1rem",
-                                        fontWeight: "bold",
-                                    }}
-                                >
-                                    What is ACM?
-                                </Typography>
-
-                                <Typography>
-                                    ACM is a student-run Computer Science organization at UCSB. We do
-                                    web development, research, and competitive programming.
-                                </Typography>
-                            </div>
-                        </div>
-                    </div>
-                </Container>
-                <div className={`${PageStyles.arrow}`}></div>
+                <Typography>
+                  ACM is a student-run Computer Science organization at UCSB. We
+                  do web development, research, and competitive programming.
+                </Typography>
+              </div>
             </div>
+          </div>
+        </Container>
+        <div className={`${PageStyles.arrow}`}></div>
+      </div>
 
-            <div className="mt-12 flex flex-col align-center">
-                <Container maxWidth="md">
-                    <Typography
-                        variant="h2"
-                        align="center"
-                        sx={{
-                            fontSize: "1.75rem",
-                            marginBottom: "1rem",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        How can I get involved?
-                    </Typography>
+      <div className="mt-12 flex flex-col align-center">
+        <Container maxWidth="md">
+          <Typography
+            variant="h2"
+            align="center"
+            sx={{
+              fontSize: "1.75rem",
+              marginBottom: "1rem",
+              fontWeight: "bold",
+            }}
+          >
+            How can I get involved?
+          </Typography>
 
-                    <Typography>
-                        The best way to get involved is to join our Discord server, where
-                        we post information about events in the announcements channel.
-                    </Typography>
+          <Typography>
+            The best way to get involved is to join our Discord server, where we
+            post information about events in the announcements channel.
+          </Typography>
 
-                    <br></br>
+          <br></br>
 
-                    <Typography
-                        variant="h3"
-                        sx={{
-                            fontWeight: "bold",
-                            fontSize: "1.5rem",
-                            marginBottom: "0.5rem",
-                        }}
-                    >
-                        Social Media
-                    </Typography>
+          <Typography
+            variant="h3"
+            sx={{
+              fontWeight: "bold",
+              fontSize: "1.5rem",
+              marginBottom: "0.5rem",
+            }}
+          >
+            Social Media
+          </Typography>
 
-                    <Typography>
-                        Follow us on Instagram and join our Discord!
-                    </Typography>
-                    
-                    <div
-                        style={{
-                            display: "flex",
-                            gap: "2%",
-                        }}
-                    >
-                        <Link href={"https://www.instagram.com/acm.ucsb"}>
-                        <div
-                            style={{
-                                display: "grid",
-                                placeItems: "center",
-                            }}
-                            className="duration-300 ease-in-out hover:scale-105"
-                        >
-                            <img
-                                src="/social-media/Instagram_Glyph_Gradient.svg"
-                                style={{
-                                    height: "60px",
-                                    padding: "10px",
-                                }}
-                            ></img>
-                            <Typography>Instagram</Typography>
-                        </div>
-                        </Link>
-                        <Link href={"https://discord.com/invite/p6rcUUjWaU"}>
-                        <div
-                            style={{
-                                display: "grid",
-                                placeItems: "center",
-                            }}
-                            className="duration-300 ease-in-out hover:scale-105"
-                        >
-                            <img
-                                src="/social-media/discord-mark-blue.svg"
-                                style={{
-                                    height: "60px",
-                                    padding: "10px",
-                                }}
-                            ></img>
-                            <Typography>Discord</Typography>
-                        </div>
-                        </Link>
-                    </div>
-                    <div style={{
-                        height: "3rem"
-                    }}></div>
-                    <Typography
-                        variant="h2"
-                        align="center"
-                        sx={{
-                            fontSize: "1.75rem",
-                            marginBottom: "1rem",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        Branches
-                    </Typography>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-[2rem] md:gap-[5rem]">
-                        {branchData.map((branch, index) => (
-                            <BranchCard key={index} index={index} branch={branch} />
-                        ))}
-                    </div>
-                    <div style={{
-                        height: "3rem"
-                    }}></div>
-                    <div>
-                        <Typography
-                            variant="h2"
-                            align="center"
-                            sx={{
-                                fontSize: "1.75rem",
-                                marginBottom: "1rem",
-                                fontWeight: "bold",
-                            }}
-                        >
-                            Meet Our Team
-                        </Typography>
-                        {fake_data.map((people, index) => (
-                            <NameCard key={index} people={people} index={index}></NameCard>
-                        ))}
-                    </div>
-                </Container>
-            </div >
-            <Footer />
-        </main >
-    );
+          <Typography>Follow us on Instagram and join our Discord!</Typography>
+
+          <div
+            style={{
+              display: "flex",
+              gap: "2%",
+            }}
+          >
+            <Link href={"https://www.instagram.com/acm.ucsb"}>
+              <div
+                style={{
+                  display: "grid",
+                  placeItems: "center",
+                }}
+                className="duration-300 ease-in-out hover:scale-105"
+              >
+                <img
+                  src="/social-media/Instagram_Glyph_Gradient.svg"
+                  style={{
+                    height: "60px",
+                    padding: "10px",
+                  }}
+                ></img>
+                <Typography>Instagram</Typography>
+              </div>
+            </Link>
+            <Link href={"https://discord.com/invite/p6rcUUjWaU"}>
+              <div
+                style={{
+                  display: "grid",
+                  placeItems: "center",
+                }}
+                className="duration-300 ease-in-out hover:scale-105"
+              >
+                <img
+                  src="/social-media/discord-mark-blue.svg"
+                  style={{
+                    height: "60px",
+                    padding: "10px",
+                  }}
+                ></img>
+                <Typography>Discord</Typography>
+              </div>
+            </Link>
+          </div>
+          <div
+            style={{
+              height: "3rem",
+            }}
+          ></div>
+          <Typography
+            variant="h2"
+            align="center"
+            sx={{
+              fontSize: "1.75rem",
+              marginBottom: "1rem",
+              fontWeight: "bold",
+            }}
+          >
+            Branches
+          </Typography>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-[2rem] md:gap-[5rem]">
+            {branchData.map((branch, index) => (
+              <BranchCard key={index} index={index} branch={branch} />
+            ))}
+          </div>
+          <div
+            style={{
+              height: "3rem",
+            }}
+          ></div>
+          <div>
+            <Typography
+              variant="h2"
+              align="center"
+              sx={{
+                fontSize: "1.75rem",
+                marginBottom: "1rem",
+                fontWeight: "bold",
+              }}
+            >
+              Meet Our Team
+            </Typography>
+            {fake_data.map((people, index) => (
+              <NameCard key={index} people={people} index={index}></NameCard>
+            ))}
+          </div>
+        </Container>
+      </div>
+      <Footer />
+    </main>
+  );
 }
--- src/components/shared/NameCard/NameCard.tsx
+++ src/components/shared/NameCard/NameCard.tsx
@@ -7,77 +7,81 @@ import acmLogoMask from "@public/assets/acmLogoMask.svg";
 import { StaticImageData } from "next/image";
 
 interface People {
-    id: string;
-    name: string;
-    position: string;
-    img: string | StaticImageData;
+  id: string;
+  name: string;
+  position: string;
+  img: string | StaticImageData;
 }
 
 interface NameCardProps {
-    people: People[];
-    index: number;
+  people: People[];
+  index: number;
 }
 
 const NameCard = ({ people, index }: NameCardProps) => {
-    return (
+  return (
+    <div key={index} className="grid grid-cols-1 md:grid-cols-3">
+      {people.map((people, index) => (
         <div
-            key={index}
-            className="grid grid-cols-1 md:grid-cols-3"
+          key={index}
+          className="m-1 flex items-center justify-center transition-transform duration-300 ease-in-out hover:scale-105"
         >
-            {people.map((people, index) => (
-                <div key={index} className="m-1 flex items-center justify-center transition-transform duration-300 ease-in-out hover:scale-105">
-                <div style={{
-                    marginRight: "0.1rem",
-                }}>
-                <Image 
-                style={
-                    {
-                        "maskImage": "url(/assets/acmLogoMask.svg)",
-                        "maskSize": "100%",
-                        "maskPosition": "center",
-                        "objectFit": "cover",
-                        "height": "100px",
-                        "width": "100px"
-                    }
-                }
-                src={people.img} alt={people.name} width={100} height={100} />
-                </div>
-                <div>
-                    <Typography
-                        variant="h4"
-                        sx={{
-                            fontWeight: "700",
-                            color: "black",
-                            fontSize: {
-                                xs: "1.75rem",
-                                sm: "1.75rem",
-                                md: "2rem",
-                                lg: "2.25rem",
-                            },
-                        }}
-                    >
-                        {people.name}
-                    </Typography>
-                    <Typography
-                        variant="body1"
-                        sx={{
-                            fontWeight: "500",
-                            color: "black",
-                            fontSize: {
-                                xs: "1.75rem",
-                                sm: "1.75rem",
-                                md: "2rem",
-                                lg: "2.25rem",
-                            },
-                        }}
-                    >
-                        {people.position}
-                    </Typography>
-                </div>
-                </div>
-            ))}
+          <div
+            style={{
+              marginRight: "0.1rem",
+            }}
+          >
+            <Image
+              style={{
+                maskImage: "url(/assets/acmLogoMask.svg)",
+                maskSize: "100%",
+                maskPosition: "center",
+                objectFit: "cover",
+                height: "100px",
+                width: "100px",
+              }}
+              src={people.img}
+              alt={people.name}
+              width={100}
+              height={100}
+            />
+          </div>
+          <div>
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: "700",
+                color: "black",
+                fontSize: {
+                  xs: "1.75rem",
+                  sm: "1.75rem",
+                  md: "2rem",
+                  lg: "2.25rem",
+                },
+              }}
+            >
+              {people.name}
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                fontWeight: "500",
+                color: "black",
+                fontSize: {
+                  xs: "1.75rem",
+                  sm: "1.75rem",
+                  md: "2rem",
+                  lg: "2.25rem",
+                },
+              }}
+            >
+              {people.position}
+            </Typography>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default NameCard;
