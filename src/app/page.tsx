144d3a4319f921b51d6912decebb953ae64acad9
dontran15 <donqt15@gmail.com>
2024-11-22 15:57:52 -0800

    refactored faq into new file to distinguish between faq and branchfaq

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import AboutBlurb from "../components/landing/body/AboutBlurb";
 import Branches from "../components/landing/body/BranchSection/Branches";
 import EventsSection from "../components/landing/body/EventsSection/EventsSection";
 import Footer from "../components/shared/Footer/Footer";
-import FAQ from "@/components/landing/body/FAQ";
+import FAQ from "@/components/landing/body/FAQ/FAQ";
 
 export default function Home() {
   return (
