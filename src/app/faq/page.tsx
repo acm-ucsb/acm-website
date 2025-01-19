a50d9106d4315d5e0e0f72faf5345ce61ee84a82
Don Tran <75715248+dontran15@users.noreply.github.com>
2025-01-19 13:16:35 -0800

    prettier fixes and rebasing

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,8 +1,6 @@
 import Navbar from "@/components/shared/Navbar/Navbar";
 import Footer from "@/components/shared/Footer/Footer";
-import BranchFAQ from "@/components/landing/body/FAQ/BranchFAQ";
 import FAQ from "@/components/landing/body/FAQ/FAQ";
-import ToggleFAQView from "@/components/landing/body/FAQ/ToggleFAQView";
 
 export default async function Home() {
   return (
