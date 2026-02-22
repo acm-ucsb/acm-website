511b0ee806cbda3ec4ef921c7160b47371419508
Don Tran <75715248+dontran15@users.noreply.github.com>
2026-02-22 01:14:21 -0800

    fix: prettier for hero section height change

--- src/components/interns/InternHeroSection.tsx
+++ src/components/interns/InternHeroSection.tsx
@@ -257,8 +257,14 @@ export const InternHeroSection = () => {
             to: { opacity: 1, transform: "translateY(0)" },
           },
           "@keyframes bounce": {
-            "0%, 100%": { transform: "translateY(0)", animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)" },
-            "50%": { transform: "translateY(-14px)", animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)" },
+            "0%, 100%": {
+              transform: "translateY(0)",
+              animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
+            },
+            "50%": {
+              transform: "translateY(-14px)",
+              animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+            },
           },
         }}
       >
